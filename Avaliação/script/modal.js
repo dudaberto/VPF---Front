b541e1b3bd18df74ee054659d498a31d00ca84e4
dudaberto <161066750+dudaberto@users.noreply.github.com>
2025-03-28 11:12:16 -0300

    Add files via upload

---
+++ Avaliação/script/modal.js
@@ -0,0 +1,17 @@
+var modal = document.getElementById("modaL");
+var btn = document.getElementById("modal"); 
+var span = document.getElementsByClassName("fechar")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
